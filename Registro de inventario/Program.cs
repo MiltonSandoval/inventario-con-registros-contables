0d558bb7cbb446e889bcba74c7d7ce07949e7279
Milton <sandovalmonasteriom@gmail.com>
2024-08-27 16:18:04 -0400

    Agregar archivos de proyecto.

---
+++ Registro de inventario/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Registro
+{
+    class Programa
+    {
+        static void Main(string[] args)
+        {
+            bool Controlador = true;
+            do
+            {
+                string opcion = MenuPrincipal();
+                switch (opcion)
+                {
+                    case "1":
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        break;
+                    case "4":
+                        Controlador = false;
+                        break;
+                    default:
+                        break;
+                }
+
+
+            } while (Controlador);
+
+
+
+        }
+        public static string MenuPrincipal()
+        {
+            Console.WriteLine("----------");
+            Console.Write(
+                $"1.Compra\n" +
+                $"2.Venta\n" +
+                $"3.Registro\n" +
+                $"4.Salir\n");
+            Console.WriteLine("----------");
+            Console.Write("ingrese su opcion:");
+            return Console.ReadLine();
+        }
+    }
+
+}
+
+
+
---
+++ Registro de inventario/SubMenu.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Registro_de_inventario
+{
+    class SubMenu
+    {
+        public static string Menu2()
+        {
+            Console.WriteLine("----------");
+            Console.Write(
+                $"Compra al:\n"+
+                $"1.Contado\n" +
+                $"2.Credito\n" +
+                $"3.Anticipo\n");
+            return Console.ReadLine();
+        }
+        public static string Menu3()
+        {
+            Console.WriteLine("----------");
+            Console.Write(
+                $"METODOS DE PAGO\n" +
+                $"1.Efectivo\n" +
+                $"2.Efectivo M/E\n" +
+                $"3.Banco M/N\n" +
+                $"4.Banco M/E\n" +
+                $"5.Cheque\n" +
+                $"6.Letra de cambio\n" +
+                $"0.Salir\n");
+            return Console.ReadLine();
+        }
+        public static string Menu4()
+        {
+            Console.WriteLine("----------");
+            Console.Write(
+                $"MODELOS DISPONIBLES\n" +
+                $"Modelos Hyundai\n" +
+                $"1.Grand i10\n" +
+                $"2.Creta\n" +
+                $"3.Tucson\n" +
+                $"4.Santa Fe");
+            return Console.ReadLine();
+        }
+    }
+}
